d0c1f690a0449f750304d6522eb4d421b703e570
aaabdelaziz <aiaaziz.sw@gmail.com>
2025-05-14 16:33:00 +0200

    Rename all files to use pyhton jupyter, for better and fast code demos.

--- MyPythonCourse/12-functions_HowTomain.py
+++
@@ -1,6 +0,0 @@
-def main():
-    # Your main code logic goes here
-    print("This is the main function.")
-
-if __name__ == "__main__":
-    main()
--- MyPythonCourse/jsonLoads_dump.py
+++
@@ -1,3 +0,0 @@
-# to convert string to json or json string
-# convert array list to json []
-# convert dictionray object to json {}
